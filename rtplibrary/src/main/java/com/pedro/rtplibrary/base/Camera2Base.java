ed11a4dded3c69afc2af30cf62accdac38b1f5d4
Yu-Hsuan Lin <b95505017@gmail.com>
2019-07-17 01:56:47 +0800

    workaround: add reTry API in Camera2Base
    Signed-off-by: Yu-Hsuan Lin <b95505017@gmail.com>

--- rtplibrary/src/main/java/com/pedro/rtplibrary/base/Camera2Base.java
+++ rtplibrary/src/main/java/com/pedro/rtplibrary/base/Camera2Base.java
@@ -5,12 +5,14 @@ import android.hardware.camera2.CameraCharacteristics;
 import android.media.MediaCodec;
 import android.media.MediaFormat;
 import android.os.Build;
-import androidx.annotation.RequiresApi;
 import android.util.Size;
 import android.view.MotionEvent;
 import android.view.Surface;
 import android.view.SurfaceView;
 import android.view.TextureView;
+
+import androidx.annotation.RequiresApi;
+
 import com.pedro.encoder.audio.AudioEncoder;
 import com.pedro.encoder.audio.GetAacData;
 import com.pedro.encoder.input.audio.GetMicrophoneData;
@@ -28,6 +30,7 @@ import com.pedro.rtplibrary.view.GlInterface;
 import com.pedro.rtplibrary.view.LightOpenGlView;
 import com.pedro.rtplibrary.view.OffScreenGlThread;
 import com.pedro.rtplibrary.view.OpenGlView;
+
 import java.io.IOException;
 import java.nio.ByteBuffer;
 import java.util.Arrays;
@@ -449,6 +452,11 @@ public abstract class Camera2Base implements GetAacData, GetVideoData, GetMicrop
       audioEncoder.stop();
       recordController.resetFormats();
     }
+  }
+
+  public void reTry(long delay) {
+    resetVideoEncoder();
+    reConnect(delay);
   }
 
   //re connection
